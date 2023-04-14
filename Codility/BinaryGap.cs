88a3f05b0143003831a0dbd8633d470fa54ee0da
Cory Fabian <Coryf65@users.noreply.github.com>
2023-04-14 00:20:14 -0500

    uncommented my work

--- Codility/BinaryGap.cs
+++ Codility/BinaryGap.cs
@@ -2,36 +2,39 @@ namespace Codility;
 
 public static class BinaryGap
 {
+    ///<Summary>
+    /// Solving the Binary Gap Alogo
+    ///</Summary>
     public static int Solution(int N)
     {
-
-            
-
         // 32 = 100000
         // 20 = 10100 : 1
-        // int result = 0;
-        // string binary =  Convert.ToString(N, 2);
-        
-        // // convert to an array
-        // string[] ar = binary.Split("");
+        int result = 0;
+        string binary = Convert.ToString(N, 2);
 
-        // for (int i = 0; i < ar.Length; i++)
-        // {
-        //     // check when its a 1 
-        //     if (ar[i] == "1")
-        //     {
-        //         // start counter
+        // convert to an array
+        string[] ar = binary.Split("");
 
-        //     }                        
-            
-        //     // if 1 and next is zero add to counter
+        for (int i = 0; i < ar.Length; i++)
+        {
+            // check when its a 1 
+            if (ar[i] == "1")
+            {
+                // start counter
 
-        //     // save on next 1    
-        // }
-        
-        // return 0;
+            }
+
+            // if 1 and next is zero add to counter
+
+            // save on next 1    
+        }
+
+        return result;
     }
 
+    ///<Summary>
+    /// Answer Found
+    ///</Summary>
     public static int FoundSolution(int N)
     {
         return Convert
@@ -40,7 +43,7 @@ public static class BinaryGap
         // remove leading and trailing 0s, as per requirement
         .Trim('0')
         // split the string by 1s
-        .Split(new [] { '1' })
+        .Split(new[] { '1' })
         // find the length of longest segment
         .Max(x => x.Length);
     }
